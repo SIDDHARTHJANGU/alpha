834a1cfa60e44786a37aea89ed32b8d63979e4ec
AAYUSH BISHNOI <aayushkaswan79@gmail.com>
2024-03-18 11:30:47 +0530

    make-branch

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <p className=' text-cyan-200 text-6xl'>hello siddharth</p>
+    </div>
+  )
+}
+
+export default Header
